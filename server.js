5996543e7e5843ebec052ba5e7ba0830da3f3617
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-04-29 20:50:24 -0400

    drinks_index.ejs updated

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const drinks = require('./models/drinks.js')
 
 
-app.get('/',(req,res) => {
-    res.send('Heelo Peter')
+app.get('/drinks/',(req,res) => {
+    res.render('drinks_index.ejs')
 })
 
 
