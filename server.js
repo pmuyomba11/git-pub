57c93bd249e0a637d044638e639f2a0db913f88e
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-04-29 20:23:33 -0400

    basic router setup

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/',(req,res) => {
+    res.send('Heelo Peter')
+})
+
+
+
+app.listen(port, () => {
+    console.log('Server is listening on Port :', port);
+})
